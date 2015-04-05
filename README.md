81d0efc46bb11cc76746567a9a1a7adc38ee7cc0
Dan Jenkins <dan.jenkins88@gmail.com>
2015-04-05 16:28:18 +0100

    re-arranging the extensions so theyre in one branch

---
+++ README.md
@@ -0,0 +1,18 @@
+# Screen Sharing in Firefox
+
+Firefox doesn't need an extension to give WebRTC developers access to the GUI which allows users to access screens / tabs / extensions; unlike Google Chrome. However, you do need to enable the domain you want to screenshare on. These exteniosn enable the domain, as well as telling the `respoke.js` library that the domain is enabled.
+
+There are two extensions within this repo, both built using the "Mozilla Addon SDK", but both use a different build mechanism. One uses the `cfx` mechanism, and the other uses `jpm`. The `cfx` build mechanism is deprecated, however extensions built with `jpm` need Firefox 38 and above - which is not currently "stable"
+
+Each extension has it's own READMEs with instructions on how to build your extension.
+
+## Where can I screen share?
+
+Currently Firefox only allows domains using the `https` protocol. Unlike Google Chrome, they do not currently support development using `http://localhost[:port]` - we recommend using a service such as [ngrok](https://ngrok.com/) which allow you to access your local environment with the https protocol.
+
+## License
+
+Respoke Screen Sharing Firefox extensions are licensed under the [MIT license](LICENSE).
+
+
+ 
---
+++ cfx/lib/main.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2015, Digium, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under The MIT License found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * For all details and documentation:  https://www.respoke.io
+ */
+
+'use strict';
+
+var pageMod = require('sdk/page-mod');
+var data = require("sdk/self").data;
+var prefs = require("sdk/preferences/service");
+
+var allowedUris = ['https://respoke.github.io/*'];
+var allowedDomains = ['respoke.github.io'];
+var allowedUris = ['https://1660a8b4.ngrok.com/', 'https://respoke.github.io'];//needs a better way to do this
+
+var allowedDomainsPref = 'media.getusermedia.screensharing.allowed_domains';
+var enableScreensharingPref = 'media.getusermedia.screensharing.enabled';
+
+exports.main = function (options, callbacks) {
+
+    if (options.loadReason !== 'startup') {
+        //add the domains!
+        prefs.set(enableScreensharingPref, true);
+        var domains = prefs.get(allowedDomainsPref).split(',');
+
+        allowedDomains.forEach(function (domain) {
+            if (domains.indexOf(domain) === -1) {
+                //add the domain into the allowedDomains
+                domains.push(domain);
+            }
+        });
+
+        prefs.set(allowedDomainsPref, domains.join(','));
+
+    }
+
+    pageMod.PageMod({
+        include: allowedUris,
+        contentScriptFile: data.url('./content.js'),
+        contentScriptWhen: 'end'
+    });
+
+    //load the script into any tabs that are already active
+    var tabs = require("sdk/tabs");
+    for (let tab of tabs) {
+         //this needs tidying up long term
+        if (allowedUris.indexOf(tab.url) !== -1) {
+            tab.attach({
+                contentScriptFile: data.url'./content.js'),
+            });
+        }
+    }
+
+};
+
+exports.onUnload = function(reason){
+    if (reason !== 'shutdown') {
+
+        //remove the domains we've set
+        var domains = prefs.get(allowedDomainsPref).split(',');
+
+        //remove the domains in allowedDomains from domains
+        domains = domains.filter(function(domain) {
+            return allowedDomains.indexOf(domain) === -1;
+        });
+
+        prefs.set(allowedDomainsPref, domains.join(','));
+
+    }
+}
